2c04ad44fe7357e46622574da5d3544476510488
Raghvendra Singh <raghvendrapratap.success@gmail.com>
2021-07-16 22:06:34 +0530

    Worked on adding post coding

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -3,15 +3,14 @@ import { Button, Card, Icon, Label, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 
-// import { AuthContext } from '../context/auth';
+import LikeButton from "./LikeButton";
+import DeleteButton from "./DeleteButton";
+import { AuthContext } from "../context/auth";
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) {
-  // const { user } = useContext(AuthContext);
-
-  const likePost = () => console.log("Like");
-  const commentOnPost = () => console.log("Comment");
+  const { user } = useContext(AuthContext);
 
   return (
     <Card fluid>
@@ -28,15 +27,8 @@ function PostCard({
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Button as="div" labelPosition="right" onClick={likePost}>
-          <Button color="teal" basic>
-            <Icon name="heart" />
-          </Button>
-          <Label basic color="teal" pointing="left">
-            {likeCount}
-          </Label>
-        </Button>
-        <Button as="div" labelPosition="right" onClick={commentOnPost}>
+        <LikeButton user={user} post={{ id, likes, likeCount }} />
+        <Button as="div" labelPosition="right" as={Link} to={`/posts/${id}`}>
           <Button color="blue" basic>
             <Icon name="comments" />
           </Button>
@@ -44,6 +36,7 @@ function PostCard({
             {commentCount}
           </Label>
         </Button>
+        {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,60 +1,44 @@
-import React from "react";
-import gql from "graphql-tag";
-import { Grid, Transition } from "semantic-ui-react";
+import React, { useContext } from "react";
 import { useQuery } from "@apollo/react-hooks";
-import PostCard from "../components/PostCard";
+import { Grid, Transition } from "semantic-ui-react";
 
-const Home = () => {
+import { AuthContext } from "../context/auth";
+import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
+
+function Home() {
+  const { user } = useContext(AuthContext);
   const {
     loading,
     data: { getPosts: posts },
   } = useQuery(FETCH_POSTS_QUERY);
-
   return (
-    <>
-      <Grid columns={3}>
-        <Grid.Row className="page-title">
-          <h1>Recent Posts</h1>
-        </Grid.Row>
-        <Grid.Row>
-          {loading ? (
-            <h1>Loading posts..</h1>
-          ) : (
-            <Transition.Group>
-              {posts &&
-                posts.map((post) => (
-                  <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-                    <PostCard post={post} />
-                  </Grid.Column>
-                ))}
-            </Transition.Group>
-          )}
-        </Grid.Row>
-      </Grid>
-    </>
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          <Transition.Group>
+            {posts &&
+              posts.map((post) => (
+                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                  <PostCard post={post} />
+                </Grid.Column>
+              ))}
+          </Transition.Group>
+        )}
+      </Grid.Row>
+    </Grid>
   );
-};
-
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`;
+}
 
 export default Home;
