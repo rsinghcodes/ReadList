5bf398fef97e9a04fee9a626276c36228754bf5a
Raghvendra Singh <raghvendrapratap.success@gmail.com>
2021-09-09 21:47:02 +0530

    Added server files

--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom";
-import ApolloProvider from "./ApolloProvider";
-import reportWebVitals from "./reportWebVitals";
-
-ReactDOM.render(ApolloProvider, document.getElementById("root"));
-
-reportWebVitals();
